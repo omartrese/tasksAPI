9b9c514165e3f687c12a63c048b13cc3c0a9ae10
omartrese <omarlayachi13@gmail.com>
2024-06-11 18:28:08 +0200

    signup functionality

--- routes/auth.js
+++ routes/auth.js
@@ -12,9 +12,40 @@ authRouter.use(cookieParser());
 authRouter.get('/api/signup', (req, res) => res.json({message: "pagina de registro (hacer petición tipo POST con datos: name, username y password)"}));
 authRouter.get('/api/login', (req, res) => res.json({message: "pagina de inicio de sesión (hacer petición tipo POST con datos: username y password)"}));
 
-authRouter.post('/api/signup', (req, res) => {
+authRouter.post('/api/signup', async (req, res) => {
     const {username, userId, password} = req.body;
     const hashPasswd = bcrypt.hashSync(password, 10);
+    let userIdExists = false;
+
+    try {
+
+        await database.all("SELECT * FROM users WHERE userId = ?", userId, (err, results) => {
+            if (err) {
+                res.status(402).json({"error": err.message});
+                return;
+            }
+
+            userIdExists = results.length > 0;
+        }) 
+        
+        
+        await database.run('INSERT INTO users (username, userId, password) VALUES (?, ?, ?)', [username, userId, hashPasswd], (results ,err) => {
+        
+            if(userIdExists) return res.status(400).json({message: "userId Already exists"});
+        
+            if(err) return res.status(400).json({message: "Error creating user"});
+
+            return res.status(201).json({message: "User created successfully!"});
+
+
+        });
+
+
+    } catch (err) {
+        return console.error(err);
+    }
+
+
 
 })
 
